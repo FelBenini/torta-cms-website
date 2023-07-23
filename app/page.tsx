4acafe529f0c3c0fc9442bbcdb19f1ee2c8bd683
felbenini <felbenini@gmail.com>
2023-07-23 20:57:22 -0300

    Added the header

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import HeroSection from '../components/LandingPage/HeroSection'
 
 export default function Home() {
   return (
-    <main>
+    <>
       <HeroSection />
-    </main>
+    </>
   )
 }
---
+++ components/Header/index.tsx
@@ -0,0 +1,36 @@
+'use client'
+import React from 'react'
+import styles from './styles.module.scss'
+import Link from 'next/link'
+import Image from 'next/image'
+import { FiSearch } from 'react-icons/fi'
+import { BsTwitter, BsGithub } from 'react-icons/bs'
+
+const Header = () => {
+  const handleSearch = (e: React.FormEvent) => {
+    e.preventDefault()
+  }
+  return (
+    <header className={styles.header}>
+      <nav>
+        <div>
+          <Link href='/docs'>Docs</Link>
+          <Link href='/blog'>Blog</Link>
+          <Link href='/about'>About</Link>
+          <Link href='/themes'>Themes</Link>
+        </div>
+        <h5><Image src='/torta-b&w.svg' alt='torta cms icon in grayscale' width={48} height={48}/> tortaCMS</h5>
+        <div>
+          <form onSubmit={handleSearch}>
+            <input type='text' placeholder='Search the website...' />
+            <button type='submit'><FiSearch color='#BEB8C3' size={20} /></button>
+          </form>
+          <BsTwitter size={28} />
+          <BsGithub size={28} />
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
