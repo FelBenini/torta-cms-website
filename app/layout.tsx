97f2248b02943e315aeb650ef1c431387d171ddf
felbenini <felbenini@gmail.com>
2023-07-25 11:42:19 -0300

    Loading bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,58 @@
+'use client'
 import Header from '@/components/Header'
+import NextNProgress from 'nextjs-progressbar';
+import NProgress from "nprogress";
+import { useEffect } from 'react'
 import './globals.scss'
 import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+type PushStateInput = [
+  data: any,
+  unused: string,
+  url?: string | URL | null | undefined
+];
 
-export const metadata = {
-  title: 'tortaCMS',
-  description: 'tortaCMS, the headless CMS that got your back(end)',
-}
+const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+
+  useEffect(() => {
+    NProgress.configure({ showSpinner: false });
+
+    const handleAnchorClick = (event: MouseEvent) => {
+      const targetUrl = new URL((event.currentTarget as HTMLAnchorElement).href);
+      const currentUrl = new URL(location.href);
+      if (targetUrl !== currentUrl && targetUrl.hostname === currentUrl.hostname) {
+        NProgress.start();
+      }
+    };
+
+    const handleMutation: MutationCallback = () => {
+      const anchorElements = document.querySelectorAll("a");
+      anchorElements.forEach((anchor) =>
+        anchor.addEventListener("click", handleAnchorClick)
+      );
+    };
+
+    const mutationObserver = new MutationObserver(handleMutation);
+    mutationObserver.observe(document, { childList: true, subtree: true });
+
+    window.history.pushState = new Proxy(window.history.pushState, {
+      apply: (target, thisArg, argArray: PushStateInput) => {
+        NProgress.done();
+        return target.apply(thisArg, argArray);
+      },
+    });
+  });
+
   return (
     <html lang="en">
       <body className={inter.className}>
+        <NextNProgress color='#eb10f3' />
         <Header />
         <main>
           {children}
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,11 @@ import About from '@/components/LandingPage/About'
 import HeroSection from '../components/LandingPage/HeroSection'
 import Connector from '@/components/Connector'
 
+export const metadata = {
+  title: 'tortaCMS',
+  description: 'tortaCMS, the headless CMS that got your back(end)',
+}
+
 export default function Home() {
   return (
     <>
