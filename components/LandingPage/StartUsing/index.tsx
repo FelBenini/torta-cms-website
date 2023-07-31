78eedd588295fc12c8352718940a9cab07889df2
felbenini <felbenini@gmail.com>
2023-07-31 18:36:51 -0300

    start using section

--- components/LandingPage/StartUsing/index.tsx
+++ components/LandingPage/StartUsing/index.tsx
@@ -1,6 +1,7 @@
 'use client'
 import styles from './styles.module.scss'
 import React, {useState, useEffect} from 'react'
+import Image from 'next/image'
 
 const StartUsing = () => {
   const [colors, setColors] = useState({
@@ -9,12 +10,14 @@ const StartUsing = () => {
     third: '',
     bg: ''
   })
+  const [src, setSrc] = useState('/torta-afternoon.svg')
   const [title, setTitle] = useState('tonight')
 
   useEffect(() => {
     const time = new Date(Date.now())
     console.log('use effect')
     if (time.getHours() < 13) {
+      setSrc('/torta-day.svg')
       setColors({
         primary: '#EC9C1D',
         secondary: '#EB7619',
@@ -23,6 +26,7 @@ const StartUsing = () => {
       })
       setTitle('this morning')
     } else if (time.getHours() < 19) {
+      setSrc('/torta-afternoon.svg')
       setColors({
         primary: '#FF456B',
         secondary: '#F31168',
@@ -31,6 +35,7 @@ const StartUsing = () => {
       })
       setTitle('this afternoon')
     } else {
+      setSrc('/torta-night.svg')
       setColors({
         primary: '#2AB1EB',
         secondary: '#0D5AD9',
@@ -43,8 +48,13 @@ const StartUsing = () => {
   return (
     <section className={styles.startUsignSection} style={{background: `radial-gradient(${colors.bg} 0%, transparent 70%)`}}>
       <div className={styles.container}>
-        <h1>Start using tortaCMS<br/><b style={{color: colors.primary, filter: `drop-shadow(0px 0px 12px ${colors.third}79)`}}>{title}</b></h1>
-        <button style={{background: `linear-gradient(to top left, ${colors.primary}, ${colors.secondary})`}}>See the Installation Guide</button>
+        <div>
+          <h1>Start using tortaCMS<br/><b style={{color: colors.primary, filter: `drop-shadow(0px 0px 12px ${colors.third}79)`}}>{title}</b></h1>
+          <button style={{background: `linear-gradient(to top left, ${colors.primary}, ${colors.secondary})`}}>See the Installation Guide</button>
+        </div>
+        <div className={styles.tortaLogo}>
+        <Image src={src} alt='tortaCMS logo' layout='fill' />
+      </div>
       </div>
     </section>
   )
