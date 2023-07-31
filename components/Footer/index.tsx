1a994dd850134d0fa01a9deb610916d4a896096d
felbenini <felbenini@gmail.com>
2023-07-31 18:54:03 -0300

    Footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import styles from './styles.module.scss'
 import Image from 'next/image'
+import { FaLinkedin } from 'react-icons/fa'
+import { BsTwitter } from 'react-icons/bs'
+import { BsGithub } from 'react-icons/bs'
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div>
         <h2><Image src='/torta-b&w.svg' alt='tortaCMS logo' width={80} height={80} />tortaCMS</h2>
+        <span>
+          <BsTwitter size={34} />
+          <FaLinkedin size={36} />
+          <BsGithub size={34} />
+        </span>
       </div>
     </footer>
   )
