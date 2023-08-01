4be5cfe14a8e35ba51f6ce0e44bd1eeb436d57d1
felbenini <felbenini@gmail.com>
2023-07-31 21:07:29 -0300

    Added about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { serialize } from 'next-mdx-remote/serialize'
+import { MDXRemote } from 'next-mdx-remote/rsc'
+import styles from './styles.module.scss'
+import Link from 'next/link'
 
-const About = () => {
+export const revalidate = 10
+
+const fetchContent = async () => {
+  const res = await fetch(`${process.env.TORTACMS_HOST}/api/page/About-tortaCMS`)
+  const json = await res.json()
+  return json
+}
+
+const components = {
+  a: (props: any) => <Link {...props} />
+}
+
+const About = async () => {
+  const post = await fetchContent()
   return (
-    <div>About</div>
+    <section className={styles.aboutSection}>
+      <div>
+        <h1>{post.title}</h1>
+        <MDXRemote components={components} source={post.content}/>
+      </div>
+    </section>
   )
 }
 
