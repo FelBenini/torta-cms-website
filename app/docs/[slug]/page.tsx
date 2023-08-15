c9e18cb1430403e640fb9b67d3b607af7d1fccfa
FelBenini <felbenini@gmail.com>
2023-08-15 19:29:55 -0300

    Docs content page

---
+++ app/docs/[slug]/page.tsx
@@ -0,0 +1,32 @@
+import MarkdownParser from '@/components/Markdown'
+import { notFound } from 'next/navigation'
+import React from 'react'
+
+export const revalidate = 10
+
+type Props = {
+  params: {
+    slug: string
+  }
+}
+
+const fetchData = async (slug: string) => {
+  const res = await fetch(`${process.env.TORTACMS_HOST}/api/page/${slug}`)
+  if (res.status !== 200) {
+    return null
+  }
+  const data = await res.json()
+  return data
+}
+
+const DocPage = async (props: Props) => {
+  const data = await fetchData(props.params.slug)
+  if (!data) {
+    return notFound()
+  }
+  return (
+    <MarkdownParser source={data.content} />
+  )
+}
+
+export default DocPage
