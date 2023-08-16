596edacf50b46b33dec3508162bfcb335c9b2a71
FelBenini <felbenini@gmail.com>
2023-08-15 21:43:49 -0300

    Change of title on side menu of docs

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -18,7 +18,7 @@ const DocsLayout = ({
         <MenuLink title='Installation with Postgres' url='/docs/postgres-installation' />
         <MenuLink title='Installation with MySQL' url='/docs/mysql-installation' />
         <h5><BiLogoDocker size={24} style={{marginBottom: -6}} /> Docker</h5>
-        <MenuLink title='Docker Images' url='/docs/docker-images' />
+        <MenuLink title='Running tortaCMS on Docker' url='/docs/docker-images' />
         <h5><AiFillApi size={20} style={{marginBottom: -4}} /> API Implementation</h5>
         <MenuLink title='Quick Guide' url='/docs/docker-images' />
         <MenuLink title='Retrieving Posts' url='/docs/docker-images' />
